3e7fd2d785be132a90470a0560586dc8f7582fb7
ZhengHongChuang <2505460258@qq.com>
2024-05-08 00:19:07 +0800

    Update README

--- test.py
+++
@@ -1,9 +0,0 @@
-# import os 
-# fn = "aaaa/videos/test.mp3"
-# ext = os.path.splitext(fn)[1][1:]
-# print(ext)
-import re
-text = "[1,00:00]   我的名字是 Video Cut"
-# m = re.match(r"\[(\d+)", text.strip())
-m = re.match(r"\[(\d+)", text.strip())  # 使用正则表达式匹配任务序号
-print(m.groups()[0])
